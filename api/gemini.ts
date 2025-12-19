cfe4c5e4dcbf498eaa34bacb693d38a5b22f38a4
hoanganhchung1982 <43825710+hoanganhchung1982@users.noreply.github.com>
2025-12-20 01:31:47 +0700

    Update gemini.ts

--- api/gemini.ts
+++ api/gemini.ts
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: 'edge',
+  regions: ['sin1'], // Chạy tại Singapore
+};
 import { GoogleGenerativeAI, SchemaType } from "@google/generative-ai";
 
 export const config = {
